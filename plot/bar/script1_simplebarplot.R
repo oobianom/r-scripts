9d48bff4d6a383e946ebf41038b41342fcdfeb25
Obi Obianom <oobianom@umaryland.edu>
2024-06-22 20:57:35 -0400

    Create script1_simplebarplot.R

---
+++ plot/bar/script1_simplebarplot.R
@@ -0,0 +1,43 @@
+############################################################################
+############################################################################
+##  Document Path: C:/...
+##
+##  Author: Obinna Obianom
+##
+##  Date: 2024-06-22
+##
+##  Title: A simple bar plot
+##
+##  Description:
+##
+##  Required Files:
+##
+##  Exported Files:
+##
+##  R Version: R version 4.0.2 (2020-06-22)
+##
+#############################################################################
+#############################################################################
+
+# clear console and unload all packages to maintain script consistency
+quickcode::clean(
+  setwd = './', # set working directory
+  source = c(), # script paths to be sourced in
+  load = c(), # RData files to load in
+  clearPkgs = TRUE # clear all packages
+)
+
+# load required packages
+quickcode::libraryAll(
+  ggplot2 # package for plotting
+)
+
+
+# script body
+
+
+
+
+
+# session information
+  sessionInfo()
